d3cb391660a6e62c1ccdf672badd726ff8456904
James-HoneyBadger <James-HoneyBadger@users.noreply.github.com>
2025-12-28 20:59:22 -0600

    Ignore Rust target and remove artifacts

--- rust/target/debug/deps/sagacraft_player-79fb8dc4e7cfffb8.d
+++
@@ -1,5 +0,0 @@
-/home/james/SagaCraft/rust/target/debug/deps/sagacraft_player-79fb8dc4e7cfffb8.d: sagacraft_player/src/main.rs
-
-/home/james/SagaCraft/rust/target/debug/deps/sagacraft_player-79fb8dc4e7cfffb8: sagacraft_player/src/main.rs
-
-sagacraft_player/src/main.rs:
